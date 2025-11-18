cc69473c4e02fd2df8138a737f75d03cdb1d0098
AnupJathar <jatharanup@gmail.com>
2025-11-18 22:31:03 +0530

    Add files via upload

---
+++ Classwork/Day_04/program54.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int CheckPrime(int iNo)
+{
+    int iCnt = 0, iFrequency = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    for(iCnt = 2; iCnt <= (iNo / 2); iCnt++)
+    {
+        if(iNo % iCnt == 0)
+        {
+            iFrequency++;       
+        }
+    }
+
+    if(iFrequency == 0)
+    {
+        return true;        //No Factors
+    }
+    else
+    {
+        return false;       //Atleast one factor
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+    bool bRet = false;
+
+    printf("Enter the number: \n");
+    scanf("%d",&iValue);
+
+    bRet = CheckPrime(iValue);
+
+    if(bRet == true)
+    {
+        printf("%d is prime number\n",iValue);
+    }
+    else
+    {
+        printf("%d is prime not number\n",iValue);
+    }
+
+    return 0;
+}
