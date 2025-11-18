3dabb0708d31ca6c2665b44c6aa672433b857782
AnupJathar <jatharanup@gmail.com>
2025-11-18 22:27:28 +0530

    Add files via upload

---
+++ Classwork/Day_07/program129.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int CountEven(int Arr[], int iSize)
+{
+    int iCnt = 0, iCount = 0;
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        if((Arr[iCnt] % 2) == 0)
+        {
+            iCount++;
+        }
+    }
+    return iCount;
+}
+
+int main()
+{
+    int iLength = 0, iCnt = 0, iRet = 0;
+    int *ptr = NULL;
+
+    printf("Enter Number of Elements : ");
+    scanf("%d",&iLength);
+
+    ptr = (int *)malloc(iLength * sizeof(int));
+    if(NULL == ptr)      // Idustrial way of coding
+    {
+        printf("Unable to allocate memory\n");
+        return -1;
+    }
+
+    printf("Enter the Elements : \n");
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d",&ptr[iCnt]);
+    }
+
+    iRet = CountEven(ptr, iLength);
+    printf("Number of Even Elements are : %d\n",iRet);
+
+    free(ptr);
+
+    return 0;
+}
---
+++ Classwork/Day_07/program132.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+float Average(int Arr[], int iSize)
+{
+    int iCnt = 0, iSum = 0;
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        iSum = iSum + Arr[iCnt];
+    }
+    return ((float)iSum / (float)iSize);
+}
+
+int main()
+{
+    int iLength = 0, iCnt = 0;
+    float fRet = 0;
+    int *ptr = NULL;
+
+    printf("Enter Number of Elements : ");
+    scanf("%d",&iLength);
+
+    ptr = (int *)malloc(iLength * sizeof(int));
+    if(NULL == ptr)      // Idustrial way of coding
+    {
+        printf("Unable to allocate memory\n");
+        return -1;
+    }
+
+    printf("Enter the Elements : \n");
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d",&ptr[iCnt]);
+    }
+
+    fRet = Average(ptr, iLength);
+    printf("Average is : %f\n",fRet);
+
+    free(ptr);
+
+    return 0;
+}
