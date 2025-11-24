64944bdf155a6a533047214f155f0ab585eeb750
AnupJathar <jatharanup@gmail.com>
2025-11-25 00:07:49 +0530

    Add files via upload

---
+++ Assignments/Assignment_26/problem1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+void Pattern(int iNo) 
+{
+    char ch = 'A';
+    int iCnt = 0;
+
+    for(iCnt = 1; iCnt <= iNo; iCnt++) 
+    {
+        printf("%c\t", ch);
+        ch++;
+    }
+    printf("\n");
+}
+
+int main() 
+{
+    int iValue = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &iValue);
+
+    Pattern(iValue);
+
+    return 0;
+}
